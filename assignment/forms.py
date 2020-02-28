cbd86356e447dc336598c3a40cf98f298688e783
aaron-beach <aaron.r.beach@gmail.com>
2020-02-28 14:44:53 -0500

    multiselect functioning

--- assignment/forms.py
+++ assignment/forms.py
@@ -5,6 +5,7 @@ from .models import *
 
 # TODO: Put your dog questions form here
 class DogForm(forms.ModelForm):
+
     class Meta:
         model = Pet
         fields = '__all__'
@@ -14,8 +15,24 @@ class DogForm(forms.ModelForm):
         }
         labels = {
             'email': 'Please provide an Email address to receive results.',
-            'trick': 'What tricks does your dog know? Select all that apply:',
             'age': 'How old is your dog?',
             'breed': 'What is the breed of your dog?',
-            'daily_walk': 'Do you walk your dog daily?'
+            'daily_walk': 'Do you walk your dog daily?',
         }
+    SIT = 'Sit'
+    FETCH = 'Fetch'
+    STAY = 'Stay'
+    ROLLOVER = 'Rollover'
+    NONE = 'None'
+    TRICK_CHOICES = [
+        (SIT, 'Sit'),
+        (FETCH, 'Fetch'),
+        (STAY, 'Stay'),
+        (ROLLOVER, 'Roll Over'),
+        (NONE, 'None. It is okay. My pup was stubborn too.'),
+    ]
+    trick = forms.MultipleChoiceField(
+        choices=TRICK_CHOICES,
+        widget=forms.CheckboxSelectMultiple,
+        label='What tricks does your dog know? Select all that apply:'
+    )
--- assignment/models.py
+++ assignment/models.py
@@ -1,28 +1,16 @@
 from django.db import models
-
+from django.contrib.postgres.fields import ArrayField
 
 # TODO: Put your dog questions model here
+
+
+def arrayfield_default_value():
+    return "NA"
+
+
 class Pet(models.Model):
-
-    SIT = 'SI'
-    FETCH = 'FT'
-    STAY = 'ST'
-    ROLLOVER = 'RO'
-    NONE = 'NA'
-    TRICK_CHOICES = [
-        (SIT, 'Sit'),
-        (FETCH, 'Fetch'),
-        (STAY, 'Stay'),
-        (ROLLOVER, 'Roll Over'),
-        (NONE, 'None. It is okay. My pup was stubborn too.'),
-    ]
-
     daily_walk = models.NullBooleanField()
     age = models.PositiveIntegerField()
     breed = models.CharField(max_length=30, blank=True)
-    trick = models.CharField(
-        max_length=2,
-        choices=TRICK_CHOICES,
-        default=NONE,
-    )
+    trick = ArrayField(models.CharField(max_length=20), default=arrayfield_default_value)
     email = models.CharField(max_length=200, blank=True)
