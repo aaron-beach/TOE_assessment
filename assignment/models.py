30a27cb0891817218aa0c11bc4925c99c1da8c42
aaron-beach <aaron.r.beach@gmail.com>
2020-02-29 14:02:06 -0500

    Abstracted email function to model

--- assignment/models.py
+++ assignment/models.py
@@ -1,5 +1,10 @@
+import re
+
+from django.core.mail import send_mail
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from toe_hiring_2020.settings import DEFAULT_FROM_EMAIL
+
 
 # TODO: Put your dog questions model here
 
@@ -14,3 +19,53 @@ class Pet(models.Model):
     breed = models.CharField(max_length=30, blank=True)
     trick = ArrayField(models.CharField(max_length=20), default=arrayfield_default_value)
     email = models.CharField(max_length=200, blank=True)
+
+    def email_user(self):
+        """
+        Sends an email to this User.
+        """
+        subject = 'Thank you!'
+
+        def email_message(form_response):
+            message = f'Thank you for taking the time to fill out the form. \n'
+
+            is_walked = form_response.cleaned_data['daily_walk']
+            if is_walked:
+                message += f'Your dog loves the walks! Trust us. \n'
+            else:
+                message += f'Hope you have a fenced in yard.\n'
+
+            breed = form_response.cleaned_data["breed"]
+            if len(breed) != 0 or breed.isspace():
+                message += f'{breed}s are so cute!!\n'
+
+            age = form_response.cleaned_data['age']
+            age_message = f'{age} years old. '
+            if age < 2:
+                age_message += f'Still a puppy. \n'
+            elif age > 9:
+                age_message += f'An old friend. \n'
+            else:
+                age_message += f'Prime of their life! \n'
+            message += age_message
+
+            trick = form_response.cleaned_data['trick']
+            trick_message = 'Tricks: \n'
+            is_none = re.search('\bNone', trick[0])
+
+            if len(trick) > 1:
+                for i in trick:
+                    trick_message += f'{i}\n'
+                trick_message += f'Impressive list. You must work really hard.'
+            elif is_none:
+                trick_message += f'{"It is okay. Tricks are not for everyone" if age > 2 else "There is still time keep trying!"}'
+            else:
+                trick_message += f'{trick[0]}\n Great start!'
+            message += trick_message
+
+            return message
+
+        #             f'{tricks if len(tricks)==1 else [i for i in tricks]}'
+        send_mail(subject, email_message(self), DEFAULT_FROM_EMAIL,
+                  [self.cleaned_data['email'], 'admin@dogstartup.com'],
+                  fail_silently=False)
--- assignment/views.py
+++ assignment/views.py
@@ -3,10 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.views.generic import *
 from django.views import View
+from .models import Pet
 from assignment.forms import *
-from django.core.mail import send_mail, BadHeaderError
-
-from toe_hiring_2020.settings import DEFAULT_FROM_EMAIL
 
 
 class Index(FormView):
@@ -28,27 +26,11 @@ class Index(FormView):
         if form.is_valid():
             form.save()
             print("saved")
-            email_contact = form.cleaned_data['email']
-            is_walked = form.cleaned_data['daily_walk']
-            breed = form.cleaned_data['breed']
-            age = form.cleaned_data['age']
-            tricks = form.cleaned_data['trick']
-            print(tricks)
-            try:
-                send_mail(
-                    'Thank you!',
-                    f'Thank you for taking the time to fill out the form. \n'
-                    f'{"Your dog loves the walks! Trust us. " if is_walked else "Hope you have a fenced in yard."}\n'
-                    f'{breed} is so cute!!\n'
-                    f'{age}{" years old. Still a puppy. " if age < 2 else " years old. An old friend. "}\n'
-                    f'{tricks if len(tricks)==1 else [i for i in tricks]}',
-
-                    DEFAULT_FROM_EMAIL,
-                    [email_contact],
-                    fail_silently=False,
-                )
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
+            Pet.email_user(form)
+            # try:
+            #
+            # except BadHeaderError:
+            #     return HttpResponse('Invalid header found.')
 
             return HttpResponseRedirect(self.get_success_url())
         else:
